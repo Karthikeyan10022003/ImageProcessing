e403649d328706f5f6cd7b8a44416d9d697ad40f
Riota IOT C21 <karthikeyan@riota.in>
2025-08-22 11:29:53 +0530

    Structured img processor files

--- image_processor.py
+++ image_processor.py
@@ -4,25 +4,23 @@ from ultralytics import YOLO
 
 def image_detection(model, image_path):
     try:
-    # Read the image
+  
         frame = cv2.imread(image_path)
 
         if frame is None:
             print(" Error: Could not read image.")
             return
 
-        # Run detection
-        results = model(frame, conf=0.1, imgsz=1280)
-
-        # Annotate the image
+        
+        results = model(frame, conf=0.5, imgsz=1280)
+      
+       
         annotated_frame = results[0].plot()
 
-        # Show the result
         cv2.imshow("Snack Detection", annotated_frame)
         cv2.waitKey(0)
         
 
-        # Save output image
         cv2.imwrite("output_image.jpg", annotated_frame)
     except(KeyboardInterrupt):
         print("Error: Keyboard interrupt detected. Exiting.")
@@ -34,8 +32,7 @@ def image_detection(model, image_path):
 
 
 def main():
-    # Load model only once
-    model = YOLO(r"D:\img_processing_trial\img_processing_trial\kitkat_lays_kurkure.pt")
+    model = YOLO(r"D:\img_processing_trial\img_processing_trial\kitkat_and_lays.pt")
     image_path=r"D:\img_processing_trial\img_processing_trial\Screenshot 2025-08-11 144949.png"
     img_link = requests.get("https://images-cdn.ubuy.co.in/63759dbdf83d7d0f39136a74-adbi-39-s-box-variety-pack-40.jpg").content
     file_name='test_input.jpg'
